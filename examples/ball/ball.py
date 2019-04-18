34570f14466f87b50eedabc89b002df20ba01019
hexthat <hexthat@gmail.com>
2019-04-18 11:12:49 -0700

    Update ball.py
    should work with any display that can be divided by 16, ie 160 x 100 wouldnt work right

--- examples/ball/ball.py
+++ examples/ball/ball.py
@@ -12,24 +12,25 @@ class Ball(stage.Sprite):
         super().update()
         self.set_frame(self.frame % 4 + 1)
         self.move(self.x + self.dx, self.y + self.dy)
-        if not 0 < self.x < 112:
+        if not 0 < self.x < (ugame.display.width - 16):
             self.dx = -self.dx
-        if not 0 < self.y < 112:
+        if not 0 < self.y < (ugame.display.height - 16):
             self.dy = -self.dy
 
 
 bank = stage.Bank.from_bmp16("ball.bmp")
-background = stage.Grid(bank)
+background = stage.Grid(bank, (ugame.display.width//16), (ugame.display.height//16))
 text = stage.Text(12, 1)
-text.move(16, 60)
+text.move(16, (ugame.display.height//2 - 4))
 text.text("Hello world!")
-ball1 = Ball(64, 0)
-ball2 = Ball(0, 76)
-ball3 = Ball(111, 64)
+ball1 = Ball((ugame.display.width//2), 0)
+ball2 = Ball(0, (ugame.display.height//2))
+ball3 = Ball((ugame.display.width//6*2), 16)
 game = stage.Stage(ugame.display, 12)
 sprites = [ball1, ball2, ball3]
 game.layers = [text, ball1, ball2, ball3, background]
-game.render_block(0, 0, 128, 128)
+game.render_block(0, 0, (ugame.display.width), (ugame.display.height))
+
 
 while True:
     for sprite in sprites:
