7744365d887df33a04e25fdec72cc610a5cb0d08
Radomir Dopieralski <openstack@sheep.art.pl>
2019-05-08 16:36:46 +0200

    Do a full reinitialization of the TFT for pybadge

--- pybadge/ugame.py
+++ pybadge/ugame.py
@@ -21,15 +21,40 @@ K_START = 0x04
 K_SELECT = 0x08
 
 # re-initialize the display for correct rotation and RGB mode
+
+_TFT_INIT = (
+    b"\x01\x80\x96" # SWRESET and Delay 150ms
+    b"\x11\x80\xff" # SLPOUT and Delay
+    b"\xb1\x03\x01\x2C\x2D" # _FRMCTR1
+    b"\xb2\x03\x01\x2C\x2D" # _FRMCTR2
+    b"\xb3\x06\x01\x2C\x2D\x01\x2C\x2D" # _FRMCTR3
+    b"\xb4\x01\x07" # _INVCTR line inversion
+    b"\xc0\x03\xa2\x02\x84" # _PWCTR1 GVDD = 4.7V, 1.0uA
+    b"\xc1\x01\xc5" # _PWCTR2 VGH=14.7V, VGL=-7.35V
+    b"\xc2\x02\x0a\x00" # _PWCTR3 Opamp current small, Boost frequency
+    b"\xc3\x02\x8a\x2a"
+    b"\xc4\x02\x8a\xee"
+    b"\xc5\x01\x0e" # _VMCTR1 VCOMH = 4V, VOML = -1.1V
+    b"\x20\x00" # _INVOFF
+    b"\x36\x01\xa8" # _MADCTL
+    # 1 clk cycle nonoverlap, 2 cycle gate rise, 3 sycle osc equalie,
+    # fix on VTL
+    b"\x3a\x01\x05" # COLMOD - 16bit color
+    b"\xe0\x10\x02\x1c\x07\x12\x37\x32\x29\x2d\x29\x25\x2B\x39\x00\x01\x03\x10" # _GMCTRP1 Gamma
+    b"\xe1\x10\x03\x1d\x07\x06\x2E\x2C\x29\x2D\x2E\x2E\x37\x3F\x00\x00\x02\x10" # _GMCTRN1
+    b"\x13\x80\x0a" # _NORON
+    b"\x29\x80\x64" # _DISPON
+)
 displayio.release_displays()
 _tft_spi = busio.SPI(clock=board.TFT_SCK, MOSI=board.TFT_MOSI)
 _tft_spi.try_lock()
 _tft_spi.configure(baudrate=24000000)
 _tft_spi.unlock()
 _fourwire = displayio.FourWire(_tft_spi, command=board.TFT_DC,
-                               chip_select=board.TFT_CS)
-display = displayio.Display(_fourwire, b'\x36\x01\xa8', width=160, height=128,
+                               chip_select=board.TFT_CS, reset=board.TFT_RST)
+display = displayio.Display(_fourwire, _TFT_INIT, width=160, height=128,
                             rotation=0, backlight_pin=board.TFT_LITE)
+del _TFT_INIT
 display.auto_brightness = True
 
 buttons = gamepadshift.GamePadShift(
