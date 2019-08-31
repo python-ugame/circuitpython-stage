a9bc140047c90d79c7b4d60b98d6195ac781b9a3
Radomir Dopieralski <openstack@sheep.art.pl>
2019-08-31 23:52:56 +0200

    Add support for PewPew M4

---
+++ pewpew_m4/ugame.py
@@ -0,0 +1,32 @@
+"""
+A helper module that initializes the display and buttons for the PewPew M4
+game console.
+"""
+
+import board
+import digitalio
+import gamepad
+import stage
+
+
+K_X = 0x01
+K_DOWN = 0x02
+K_LEFT = 0x04
+K_RIGHT = 0x08
+K_UP = 0x10
+K_O = 0x20
+K_START = 0x40
+K_SELECT = 0x00
+
+
+display = board.DISPLAY
+buttons = gamepad.GamePad(
+    digitalio.DigitalInOut(board.BUTTON_X),
+    digitalio.DigitalInOut(board.BUTTON_DOWN),
+    digitalio.DigitalInOut(board.BUTTON_LEFT),
+    digitalio.DigitalInOut(board.BUTTON_RIGHT),
+    digitalio.DigitalInOut(board.BUTTON_UP),
+    digitalio.DigitalInOut(board.BUTTON_O),
+    digitalio.DigitalInOut(board.BUTTON_Z),
+)
+audio = stage.Audio(board.P5)
