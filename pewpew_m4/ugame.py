8d5cc384058b1cb296aaeab86fb8405042d547ed
Radomir Dopieralski <openstack@sheep.art.pl>
2019-12-26 21:51:14 +0100

    Reset PewPew M4 when Z button is held for 5s

--- pewpew_m4/ugame.py
+++ pewpew_m4/ugame.py
@@ -2,6 +2,7 @@ import board
 import digitalio
 import gamepad
 import stage
+import time
 
 
 K_X = 0x01
@@ -15,14 +16,34 @@ K_Z = 0x40
 K_SELECT = 0x00
 
 
+class _Buttons:
+    def __init__(self):
+        self.buttons = gamepad.GamePad(
+            digitalio.DigitalInOut(board.BUTTON_X),
+            digitalio.DigitalInOut(board.BUTTON_DOWN),
+            digitalio.DigitalInOut(board.BUTTON_LEFT),
+            digitalio.DigitalInOut(board.BUTTON_RIGHT),
+            digitalio.DigitalInOut(board.BUTTON_UP),
+            digitalio.DigitalInOut(board.BUTTON_O),
+            digitalio.DigitalInOut(board.BUTTON_Z),
+        )
+        self.last_z_press = None
+
+    def get_pressed(self):
+        buttons = self.buttons.get_pressed()
+        if buttons & K_Z:
+            now = time.monotonic()
+            if self.last_z_press:
+                if now - self.last_z_press > 5:
+                    import microcontroller
+                    microcontroller.reset()
+            else:
+                self.last_z_press = now
+        else:
+            self.last_z_press = None
+        return buttons
+
+
 display = board.DISPLAY
-buttons = gamepad.GamePad(
-    digitalio.DigitalInOut(board.BUTTON_X),
-    digitalio.DigitalInOut(board.BUTTON_DOWN),
-    digitalio.DigitalInOut(board.BUTTON_LEFT),
-    digitalio.DigitalInOut(board.BUTTON_RIGHT),
-    digitalio.DigitalInOut(board.BUTTON_UP),
-    digitalio.DigitalInOut(board.BUTTON_O),
-    digitalio.DigitalInOut(board.BUTTON_Z),
-)
+buttons = _Buttons()
 audio = stage.Audio(board.P5)
