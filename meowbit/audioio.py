3fa977caa0cca38c2b524c93f7f17b1c3d0bb1be
Radomir Dopieralski <openstack@sheep.art.pl>
2021-08-30 18:57:40 +0200

    Add support for Meowbit

---
+++ meowbit/audioio.py
@@ -0,0 +1,13 @@
+class AudioOut:
+    def __init__(self, pin):
+        pass
+
+    def stop(self):
+        pass
+
+    def play(self, what):
+        pass
+
+class WaveFile:
+    def __init__(self, file, buffer=None):
+        pass
---
+++ meowbit/ugame.py
@@ -0,0 +1,65 @@
+import board
+import stage
+import busio
+import time
+import keypad
+
+
+K_X = 0x01
+K_O = 0x02
+K_DOWN = 0x04
+K_LEFT = 0x08
+K_RIGHT = 0x10
+K_UP = 0x20
+K_Z = 0x40
+
+display = board.DISPLAY
+display.auto_brightness = True
+display.auto_refresh = False
+
+
+class _Buttons:
+    def __init__(self):
+        self.keys = keypad.Keys((board.BTNA, board.BTNB, board.DOWN,
+            board.LEFT, board.RIGHT, board.UP),
+            value_when_pressed=False, interval=0.05)
+        self.last_state = 0
+        self.event = keypad.Event(0, False)
+        self.last_z_press = None
+
+    def get_pressed(self):
+        buttons = self.last_state
+        events = self.keys.events
+        while events:
+            if events.get_into(self.event):
+                bit = 1 << self.event.key_number
+                if self.event.pressed:
+                    buttons |= bit
+                    self.last_state |= bit
+                else:
+                    self.last_state &= ~bit
+        if buttons & K_Z:
+            now = time.monotonic()
+            if self.last_z_press:
+                if now - self.last_z_press > 2:
+                    supervisor.set_next_code_file(None)
+                    supervisor.reload()
+            else:
+                self.last_z_press = now
+        else:
+            self.last_z_press = None
+        return buttons
+
+
+class DummyAudio:
+    def play(self, f, loop=False):
+        pass
+
+    def stop(self):
+        pass
+
+    def mute(self, mute):
+        pass
+
+audio = DummyAudio()
+buttons = _Buttons()
